5c748d904a57a37a0e67a25ad544a85db1751bc8
jimin-ni <jimin326ww@gmail.com>
2023-06-28 21:54:05 +0900

    [0628] 김지민 회원가입기능 임시완

--- toyproject_1/accounts/views.py
+++ toyproject_1/accounts/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from .forms import UserCreateForm
+from django.shortcuts import redirect, render
+from django.contrib.auth.forms import UserCreationForm
+
+from .forms import UserCreateForm, SignUpForm
 
 # Create your views here.
 def signup_view(request):
@@ -10,4 +12,18 @@ def signup_view(request):
         context = { 'form': form }
         return render(request, 'accounts/signup.html', context )
     else:
-        pass
+    # POST 요청시 데이터 확인 후 회원 생성
+        form = SignUpForm(request.POST)
+        
+        if form.is_valid():
+            # 회원가입처리 가능
+            instance = form.save()
+            return redirect('index')        # 회원가입 완료시 노출 페이지
+            
+        else:
+            # 리다이렉트
+            return redirect('accounts:signup')    
+            # return redirect('index')
+    
+        
+    
