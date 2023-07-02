534301ef1a8f3e0e3565401c43300adbbeb7c21d
20210815 <kyumin991105@duksung.ac.kr>
2023-07-03 01:03:00 +0900

    [0703] 이규민 삭제 구현

--- toyproject_1/myapp/views.py
+++ toyproject_1/myapp/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Post
 from django.views.generic import (
     ListView,
     DetailView,
     CreateView,
-    DeleteView,
     UpdateView,
 )
 # Create your views here.
@@ -25,15 +25,28 @@ class PostDetail(DetailView):
 class PostUpdate(UpdateView):
     model = Post
     template_name = "myapp/fix.html"
-    fields = ["title", "content", "image"]
+    fields = ["title", "content", "image", "author"]
+
 
 
 class PostCreate(CreateView):
     model = Post
-    fields = ["title", "content", "image"]
+    fields = ["title", "content", "image", "author"]
     template_name = "myapp/create.html"
 
+    def form_valid(self, form):
+        current_user = self.request.user
+        if current_user.is_authenticated:
+            form.instance.author = current_user
+            return super(PostCreate, self).form_valid(form)
+        else:
+            return redirect('myapp:list')
 
-class PostDelete(DeleteView):
-    model = Post
-    success_url = "/"
+def PostDelete(request, pk):
+    post = get_object_or_404(Post, id=pk)
+    if request.method == "GET":
+        context = {'post': post}
+        return render(request, "myapp/delete.html", context)
+    else:
+        post.delete()
+        return redirect("myapp:list")
